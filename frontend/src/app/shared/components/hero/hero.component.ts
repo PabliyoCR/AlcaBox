136f048aaef5499be02d83d58fda6e9bb805458b
PabliyoCR <jimenezpablo94@gmail.com>
2021-11-07 19:56:24 -0600

    adding layout

---
+++ frontend/src/app/shared/components/hero/hero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'div[app-hero]',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss']
+})
+export class HeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
